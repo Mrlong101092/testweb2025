11b59f569cfc6fb5614e2205db251c3144ddc673
Mrlong101092 <mrlong01012020@gmail.com>
2025-02-08 01:29:33 +0700

    0022025

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return(
+    <div>
+      Mrlong & Hacker 2025
+    </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +25,7 @@ function App() {
         </a>
       </div>
       <h1>Hi! I'm mrlong & Hello World</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
