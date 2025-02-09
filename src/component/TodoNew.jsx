fcfd0253694cffa67427e88db29f712aeab4cea6
Mrlong101092 <mrlong01012020@gmail.com>
2025-02-09 22:26:32 +0700

    0102025

--- src/component/TodoNew.jsx
+++ src/component/TodoNew.jsx
@@ -1,12 +1,23 @@
 const TodoNew = (props) =>{
   const {addNewTodo} = props;
-  //addNewTodo("Mrlong");
+//addNewTodo("Mrlong");
+
+const handleClick = ()=>{
+  alert("click me")
+}
+const handleOnChange = (name)=>{
+  console.log(">>> handleOnChange", name)  
+}
 
   console.log(">>> check props: ", props)
     return (
         <div className='todo-new'>
-          <input type="text" />
-          <button>Add</button>
+          <input type="text" 
+            onChange={(event)=>handleOnChange(event.target.value)}
+          />
+          <button style={{cursor: "pointer"}}
+            onClick={handleClick}
+          >Add</button>
         </div>
     )
 }
